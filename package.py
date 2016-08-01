cdad1d8348c7d17e6826a3fdd0e20ca83c51dc3c
Fredrik Brannbacka <fredrik@madcrew.se>
2016-08-01 22:15:05 +0200

    Fixed package.py

--- package.py
+++ package.py
@@ -2,14 +2,10 @@ name = "ptex"
 
 version = "2.1.28"
 
-#requires = [
-#  "pyside"
-#]
-
 variants = [
     ["platform-linux"]
 ]
 
 def commands():
     env.PATH.prepend("{root}/bin")
-    env.PYTHONPATH.append("{root}/lib/python2.7/site-packages")
+    env.LD_LIBRARY_PATH.append("{root}/lib64")
