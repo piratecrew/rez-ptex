79e1213d889924d7eec7c76d0afb0ed8dbe00fd6
Fredrik Brännbacka FL <fredrik.brannbacka@ilpvfx.com>
2017-08-20 01:11:09 +0200

    Added travis conf

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rez-ptex
 Rez package for Ptex
+----
+[![Build Status](https://travis-ci.org/piratecrew/rez-ptex.svg?branch=master)](https://travis-ci.org/piratecrew/rez-ptex)
+
+Build status id for versions:
+  - 1.2.11
--- package.py
+++ package.py
@@ -1,12 +1,14 @@
 name = "ptex"
 
-version = "2.0.62"
+version = "2.1.28"
 
 variants = [
     ["platform-linux"]
 ]
 
 def commands():
-    env.CMAKE_MODULE_PATH.append("{root}/cmake")
     env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.append("{root}/lib")
+    if building:
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
+
